d599031f65f7bf6c1134e0587429786827e0be9c
Lenka Erbenova <lenka.erbenova79@gmail.com>
2020-05-07 15:43:21 +0200

    Ukol zpracovany, predem dekuji za feedback

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 'use strict';
 
 console.log('funguju!');
+
+let tasks = [];
+
+const updateTasks = () => {
+  const toDoElm = document.querySelector(".todo__tasks");
+  toDoElm.innerHTML = '';
+  for (let i = 0; i < tasks.length; i += 1) {
+    toDoElm.innerHTML +=
+      `<div class="task">${tasks[i]}</div>`
+  }
+}
+
+const addNewTask = () => {
+  const inputElm = document.querySelector(".new-task");
+  if (inputElm.value) {
+    tasks.push(inputElm.value);
+    updateTasks();
+  }
+}
+
+document.querySelector("button").addEventListener('click', addNewTask)
+
+
+
+
